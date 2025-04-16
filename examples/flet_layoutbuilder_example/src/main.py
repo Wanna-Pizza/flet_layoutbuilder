5d257dab043d2c7e8c746d55ab1bc09c6ca1ee6f
Wanna-Pizza <dimitri@screenskinz.co>
2025-04-16 07:28:51 +0300

    Refactor project structure: update .gitignore, add example app, and modify pyproject.toml

---
+++ examples/flet_layoutbuilder_example/src/main.py
@@ -0,0 +1,22 @@
+import flet as ft
+
+from flet_layoutbuilder import LayoutBuilder
+
+
+def main(page: ft.Page):
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+
+    page.add(
+        ft.Text("Resize the window to see the LayoutBuilder in action"),
+        LayoutBuilder(
+            content=ft.Container(bgcolor='red',expand=True,content=ft.Text("Hello World"),alignment=ft.alignment.center),
+            update_size_on_init=True,
+            alignment=ft.alignment.center,
+            expand=True,
+            on_change=lambda e: print(e)
+        ),
+    )
+
+
+ft.app(main)
